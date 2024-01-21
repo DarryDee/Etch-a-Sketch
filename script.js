3558ef1cecf88c3aa0a36adc23fcd21aac649419
DarryDee <darryd04@gmail.com>
2024-01-21 10:44:39 +0100

    Every div has a Event Handler.
    if the mouse hovers over the div the background will turn black.

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ for(let i = 0; i < 16;i++){
 
     for(let j = 0; j < 16; j++){
 
-       container.appendChild(document.createElement("div")).setAttribute("class","grid")
+        let grid = document.createElement("div");
+        grid.setAttribute("id",`Row-${j}Column-${i}`);
+
+        grid.addEventListener("mouseover",()=>{
+            grid.style.background = "black";
+        })
+        container.appendChild(grid);
     }
     
 }
