8f143f901f13070debfd30d9a5fbd5f1e238d20b
Andersen <darryd04@gmail.com>
2024-01-22 10:22:02 +0100

    User can choose the Squares per side.
    Due to problelm with variable scope the DrawGrid Mehtod is applied. Cant delete the grid-Container and Draw new Grids. This causes weird issues. Thats why the grids are being called by class and being deleted. After that the Grids will be drawn with the number the user chose.

--- script.js
+++ script.js
@@ -1,38 +1,63 @@
 const container = document.querySelector(".container");
-
+const choosePixel = document.createElement("button");
 const gridContainer = document.createElement("div");
+const buttonContainer = document.createElement("div");
+
+
 gridContainer.className = "grid-container";
 container.appendChild(gridContainer);
 
-const buttonContainer = document.createElement("div");
+
 buttonContainer.className = "button-container";
 container.appendChild(buttonContainer)
 
-const choosePixel = document.createElement("button");
-choosePixel.textContent = "Choose Squares per Side"
+choosePixel.textContent = "Choose Squares per Side";
+buttonContainer.appendChild(choosePixel);
 
-container.appendChild(choosePixel);
-buttonContainer.appendChild(choosePixel)
-
-
-choosePixel.addEventListener("click",()=>{
-    numberOfSquares = prompt("How many Squares do you want per side ? Give in a number between 16 and 100");
-
-})
 let numberOfSquares = 16;
-let value = (500 / Number(numberOfSquares)) - 4
+let value = (500 / Number(numberOfSquares)) - 4;
 
+DrawGrid(numberOfSquares,value);
 
-for(let i = 0; i < 16;i++){
+choosePixel.addEventListener("click",NewGrid);
 
-    for(let j = 0; j < 16; j++){
+function NewGrid(){
+    
+    numberOfSquares = prompt("How many Squares do you want per side? Give in a number between 16 and 100");
+    if (numberOfSquares < 16 || numberOfSquares > 100) {
+        alert("Invalid Input! Number should be bigger than 16 and smaller than 100");
+        return;
+    }
+    value = (500 / Number(numberOfSquares)) - 4;
+    clearGrid();
+    DrawGrid(numberOfSquares,value);
+    
+}
 
-        let grid = document.createElement("div");
-        grid.setAttribute("id",`Row-${j}Column-${i}`);
+function clearGrid(){
+    let divs = document.querySelectorAll(".grids");
+    divs.forEach(div =>{
+        div.remove();
+    })
+    
+}
+function DrawGrid(numberOfSquares, value){
 
-        grid.addEventListener("mouseover",()=>{
-            grid.style.background = "black";
-        })
-        gridContainer.appendChild(grid);
+    for(let i = 0; i < Number(numberOfSquares);i++){
+
+        for(let j = 0; j < Number(numberOfSquares); j++){
+    
+            let grid = document.createElement("div");
+            grid.setAttribute("id",`Row-${j}Column-${i}`);
+            grid.setAttribute("class", "grids")
+    
+            grid.style.width = value.toString() + "px";
+            grid.style.height = value.toString() + "px";
+    
+            grid.addEventListener("mouseover",()=>{
+                grid.style.background = "black";
+            })
+            gridContainer.appendChild(grid);
+        }
     }
 }
