e8637fbb155b3fe58bf47ccd7ab17d35c5b6509c
Andersen <darryd04@gmail.com>
2024-01-23 00:32:43 +0100

    Added a new Container for user settings and moved the buttons into the container. -new Button to reset the grid to 16 x 16 -Added a visual color picker interface so that the user can change the background color to its preferred color other than black.

--- script.js
+++ script.js
@@ -2,27 +2,41 @@ const container = document.querySelector(".container");
 const choosePixel = document.createElement("button");
 const gridContainer = document.createElement("div");
 const buttonContainer = document.createElement("div");
-
+const settingcontainer = document.createElement("div");
+const resetButton = document.createElement("button");
+const selectColor = document.createElement("input");
 
 gridContainer.className = "grid-container";
 container.appendChild(gridContainer);
 
-
-buttonContainer.className = "button-container";
-container.appendChild(buttonContainer)
+document.body.appendChild(settingcontainer);
+settingcontainer.setAttribute("class", "setting-container")
 
 choosePixel.textContent = "Choose Squares per Side";
-buttonContainer.appendChild(choosePixel);
+settingcontainer.appendChild(choosePixel);
+
+document.body.appendChild(settingcontainer);
+settingcontainer.setAttribute("class", "setting-container")
+
+selectColor.type = "color";
+selectColor.value = "#FF0000";
+settingcontainer.appendChild(selectColor);
+
+settingcontainer.appendChild(resetButton);
+resetButton.textContent = "Reset Grid"
+
 
 let numberOfSquares = 16;
 let value = (500 / Number(numberOfSquares)) - 4;
 
+
+//Default Grid
 DrawGrid(numberOfSquares,value);
 
 choosePixel.addEventListener("click",NewGrid);
 
 function NewGrid(){
-    
+
     numberOfSquares = prompt("How many Squares do you want per side? Give in a number between 16 and 100");
     if (numberOfSquares < 16 || numberOfSquares > 100) {
         alert("Invalid Input! Number should be bigger than 16 and smaller than 100");
@@ -34,12 +48,25 @@ function NewGrid(){
     
 }
 
+selectColor.addEventListener("change", (event)=>{
+    selectColor.value = event.target.value;
+})
+
+resetButton.addEventListener("click", resetGrid);
+
+function resetGrid(){
+    let numberOfSquares = 16;
+    let value = (500 / Number(numberOfSquares)) - 4;
+
+    clearGrid();
+    DrawGrid(numberOfSquares,value)
+}
+
 function clearGrid(){
     let divs = document.querySelectorAll(".grids");
     divs.forEach(div =>{
         div.remove();
     })
-    
 }
 function DrawGrid(numberOfSquares, value){
 
@@ -55,7 +82,7 @@ function DrawGrid(numberOfSquares, value){
             grid.style.height = value.toString() + "px";
     
             grid.addEventListener("mouseover",()=>{
-                grid.style.background = "black";
+                grid.style.background = selectColor.value;
             })
             gridContainer.appendChild(grid);
         }
